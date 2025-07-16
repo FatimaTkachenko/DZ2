4a1e4f56c054f65435442c22f0fd768de2849cfe
Мой компьютер <fatitkacenko@gmail.com>
2025-07-16 16:14:39 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int ticketPrice = 735; //стоимость билета
         int deposit = 735; //пополнили счет
-        int miles = 20;
+        int numberOfMiles = 20;
 
         System.out.println(updated / miles);
     }
